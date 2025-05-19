a09b73c44dc735d515c440fb59dee811e1580395
Jonathan Sada <jonathansada@gmail.com>
2025-05-19 16:09:10 +0200

    Final Solution

--- solutions.sql
+++ solutions.sql
@@ -26,16 +26,16 @@ SELECT DISTINCT(k_symbol) FROM `order` WHERE k_symbol <> " "; -- I excluded the 
 SELECT order_id FROM `order` WHERE account_id=34;
 
 -- Q10 - In the order table, which account_ids were responsible for orders between order_id 29540 and order_id 29560 (inclusive)?
-SELECT account_id FROM `order` WHERE order_id>=29540 AND order_id <=29560 GROUP BY account_id;
+SELECT DISTINCT(account_id) FROM `order` WHERE order_id>=29540 AND order_id <=29560;
 
 -- Q11 - In the order table, what are the individual amounts that were sent to (account_to) id 30067122?
 SELECT amount FROM `order` WHERE account_to = 30067122;
 
 -- Q12 - In the trans table, show the trans_id, date, type and amount of the 10 first transactions from account_id 793 in chronological order, from newest to oldest.
-SELECT trans_id, date, type, amount FROM trans WHERE account_id = 793 ORDER BY trans_id DESC LIMIT 10;
+SELECT trans_id, date, type, amount FROM trans WHERE account_id = 793 ORDER BY date DESC LIMIT 10;
 
 -- Q13 - In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
-SELECT district_id, COUNT(client_id) FROM client WHERE district_id < 10 GROUP BY district_id ORDER BY district_id ASC;
+SELECT district_id, COUNT(client_id) as num_clients FROM client WHERE district_id < 10 GROUP BY district_id ORDER BY district_id ASC;
 
 -- Q14 - In the card table, how many cards exist for each type? Rank the result starting with the most frequent type.
 SELECT type, COUNT(card_id) as num_cards FROM card GROUP BY type ORDER BY num_cards DESC;
@@ -47,7 +47,7 @@ SELECT account_id, SUM(amount) as total_amount FROM loan GROUP BY account_id ORD
 SELECT date, COUNT(loan_id) FROM loan WHERE date < 930907 GROUP BY date ORDER BY date DESC;
 
 -- Q17 - In the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration, ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
-SELECT date, duration, COUNT(loan_id) FROM loan WHERE date LIKE '9712%' GROUP BY date, duration ORDER BY date ASC;
+SELECT date, duration, COUNT(loan_id) as loan_count FROM loan WHERE date LIKE '9712%' GROUP BY date, duration ORDER BY date ASC;
 
 -- Q18 - In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type.
-SELECT account_id, type, SUM(amount) as total_ammount FROM trans WHERE account_id = 396 AND (type="PRIJEM" OR  type="VYDAJ") GROUP BY type; 
+SELECT account_id, type, SUM(amount) as total_amount FROM trans WHERE account_id = 396 AND (type="PRIJEM" OR  type="VYDAJ") GROUP BY type; 
