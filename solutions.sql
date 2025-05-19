22a9709e41740a75a91e198b12a407f8838064ed
Jonathan Sada <jonathansada@gmail.com>
2025-05-19 16:00:53 +0200

    Solution Improvements 2

--- solutions.sql
+++ solutions.sql
@@ -32,7 +32,7 @@ SELECT account_id FROM `order` WHERE order_id>=29540 AND order_id <=29560 GROUP 
 SELECT amount FROM `order` WHERE account_to = 30067122;
 
 -- Q12 - In the trans table, show the trans_id, date, type and amount of the 10 first transactions from account_id 793 in chronological order, from newest to oldest.
-SELECT trans_id, date, type, amount FROM trans WHERE account_id = 793 ORDER BY date DESC LIMIT 10;
+SELECT trans_id, date, type, amount FROM trans WHERE account_id = 793 ORDER BY trans_id DESC LIMIT 10;
 
 -- Q13 - In the client table, of all districts with a district_id lower than 10, how many clients are from each district_id? Show the results sorted by the district_id in ascending order.
 SELECT district_id, COUNT(client_id) FROM client WHERE district_id < 10 GROUP BY district_id ORDER BY district_id ASC;
@@ -47,7 +47,7 @@ SELECT account_id, SUM(amount) as total_amount FROM loan GROUP BY account_id ORD
 SELECT date, COUNT(loan_id) FROM loan WHERE date < 930907 GROUP BY date ORDER BY date DESC;
 
 -- Q17 - In the loan table, for each day in December 1997, count the number of loans issued for each unique loan duration, ordered by date and duration, both in ascending order. You can ignore days without any loans in your output.
-SELECT date, duration, COUNT(loan_id) FROM loan WHERE date >=971201 AND date <=971231 GROUP BY date, duration ORDER BY date ASC;
+SELECT date, duration, COUNT(loan_id) FROM loan WHERE date LIKE '9712%' GROUP BY date, duration ORDER BY date ASC;
 
 -- Q18 - In the trans table, for account_id 396, sum the amount of transactions for each type (VYDAJ = Outgoing, PRIJEM = Incoming). Your output should have the account_id, the type and the sum of amount, named as total_amount. Sort alphabetically by type.
-SELECT account_id, type, SUM(amount) FROM trans WHERE account_id = 396 AND (type="PRIJEM" OR  type="VYDAJ") GROUP BY type; 
+SELECT account_id, type, SUM(amount) as total_ammount FROM trans WHERE account_id = 396 AND (type="PRIJEM" OR  type="VYDAJ") GROUP BY type; 
